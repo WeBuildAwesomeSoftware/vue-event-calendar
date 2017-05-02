1387efddb60a2455782a8b39462b78cebe5bfcd8
jacob zlogar <jacob.zlogar@gmail.com>
2017-05-02 15:25:46 -0400

    changed the format of the dates

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ export default {
     dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     format: 'MM/yyyy',
-    fullFormat: 'dd/MM/yyyy',
+    fullFormat: 'MM/dd/yyyy',
     dayEventsTitle: 'All Events',
     notHaveEvents: 'Not Have Events'
   },
