3895f854abc07c213ef484ff191095dddcf872eb
jacob zlogar <jacob.zlogar@gmail.com>
2017-05-02 15:26:00 -0400

    webpack

--- dist/index.js
+++ dist/index.js
@@ -205,7 +205,7 @@ module.exports = function normalizeComponent (
     dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     format: 'MM/yyyy',
-    fullFormat: 'dd/MM/yyyy',
+    fullFormat: 'MM/dd/yyyy',
     dayEventsTitle: 'All Events',
     notHaveEvents: 'Not Have Events'
   },
